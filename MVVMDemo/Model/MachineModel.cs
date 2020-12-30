d4a09745268da8edf8d16b5a506cd0e2f275e7aa
Angela Marie Miranda <angelamiranda1211@gmail.com>
2020-12-30 20:10:01 +0800

    Add MachineModel and Material Themes. Also fixed design for Customer Records

---
+++ MVVMDemo/Model/MachineModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.Model
+{
+    class MachineModel
+    {
+        public string machineId { get; set; }
+        public string clusterNumber { get; set; }
+        public string controllerIpAddress { get; set; }
+
+    }
+}
